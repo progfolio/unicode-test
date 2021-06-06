17163c8919966008a8b81d358d99f62c5b0bb36c
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2021-06-06 07:54:06 -0400

    Unicode file name

---
+++ 测试.el
@@ -0,0 +1,11 @@
+;;; unicode-test.el --- Unicode test for straight.el  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;; 
+
+;;; Code:
+
+(defun 測試 () "Test function." t)
+(provide '测试)
+
+;;; unicode-test.el ends here
